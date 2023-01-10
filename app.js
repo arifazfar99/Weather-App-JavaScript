5b5fef07c91e62e2995876e7bd56f7d6cf501e16
arifazfar99 <arifazfar99@gmail.com>
2023-01-10 22:06:08 +0800

    commit

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ function showError(error) {
 
 //get weather form API
 function getWeather(latitude, longitude) {
-  let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+  let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
 
   fetch(api)
     .then(function (response) {
